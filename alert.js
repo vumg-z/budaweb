9d4afcf16fa4b1e37377f2f36055e05dda8566c7
vumg <ab17100222@gmail.com>
2023-04-13 11:22:53 -0600

    alert test

--- alert.js
+++ alert.js
@@ -2,7 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const infoButton = document.getElementById('infoButton');
     const card = document.getElementById('card');
 
+    function isClickOutsideCard(event, cardElement) {
+        return !cardElement.contains(event.target) && event.target !== infoButton;
+    }
+
     infoButton.addEventListener('click', function () {
         card.classList.toggle('card-visible');
+    });
+
+    document.addEventListener('click', function (event) {
+        if (card.classList.contains('card-visible') && isClickOutsideCard(event, card)) {
+            card.classList.remove('card-visible');
+        }
     });
 });
